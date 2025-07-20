2fce63edf0fd2681c665dff40dcad1f5b8090ed6
Md.Rafiqul Islam <rafiqul.islam@brainstation-23.com>
2025-07-21 01:53:34 +0600

    add api service and Retrofit instance

---
+++ app/src/main/java/dev/rafiqulislam/movieapps/retrofit/ApiService.kt
@@ -0,0 +1,14 @@
+package dev.rafiqulislam.movieapps.retrofit
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("movie/popular")
+    suspend fun getPopularMovies(
+        @Query("api_key")
+        apiKey: String
+    ):MovieResponse
+
+
+}
